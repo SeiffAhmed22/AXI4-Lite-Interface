c740f87acdff6b3abf3310da315ca0111547d76b
SeiffAhmed22 <seif.ahmed59122@gmail.com>
2024-10-19 21:59:25 +0300

    Updated ports and reset added

--- axi4_lite_master.v
+++ axi4_lite_master.v
@@ -6,6 +6,14 @@ module axi4_lite_master #(
     input ACLK,
     input ARESETN, // Active low reset
 
+    // Start Signals
+    input start_read,
+    input start_write,
+
+    // External ports
+    input [ADDRESS_WIDTH - 1:0] address,
+    input [DATA_WIDTH - 1:0] data,
+
     //Read Address Channel
     input M_AXI_ARREADY,
     output reg [ADDRESS_WIDTH - 1:0] M_AXI_ARADDR,
@@ -17,6 +25,11 @@ module axi4_lite_master #(
     input M_AXI_RVALID,
     output reg M_AXI_RREADY,
 
+    // Write Address Channel
+    output [ADDRESS_WIDTH - 1:0] M_AXI_AWADDR,
+    output M_AXI_AWVALID,
+    input M_AXI_AWREADY,
+
     // Write Data Channel
     output reg [DATA_WIDTH - 1:0] M_AXI_WDATA,
     output reg [DATA_WIDTH/8 - 1:0] M_AXI_WSTRB,
@@ -26,5 +39,17 @@ module axi4_lite_master #(
     input M_AXI_BVALID,
     output reg M_AXI_BREADY
 );
-    
+  localparam    IDLE = 3'b000,
+                RADDR_CHANNEL = 3'b001,
+                RDATA_CHANNEL = 3'b010,
+                WRITE_CHANNEL = 3'b011,
+                WRESP__CHANNEL = 3'b100;
+
+  reg current_state, next_state;
+
+  always @(posedge ACLK or negedge ARESETN) begin
+    if (!ARESETN) current_state <= IDLE;
+    else current_state <= next_state;
+  end
+
 endmodule
--- axi4_lite_slave.v
+++ axi4_lite_slave.v
@@ -1,6 +1,7 @@
 module axi4_lite_slave #(
     parameter ADDRESS_WIDTH = 32,
-    parameter DATA_WIDTH = 32
+    parameter DATA_WIDTH = 32,
+    parameter DATA_DEPTH = 32
 ) (
     // Global Inputs
     input ACLK,
@@ -17,6 +18,11 @@ module axi4_lite_slave #(
     output reg S_AXI_RVALID,
     input S_AXI_RREADY,
 
+    // Write Address Channel
+    input [ADDRESS_WIDTH - 1:0] S_AXI_AWADDR,
+    input S_AXI_AWVALID,
+    output S_AXI_AWREADY,
+
     // Write Data Channel
     input [DATA_WIDTH - 1:0] S_AXI_WDATA,
     input [DATA_WIDTH/8 - 1:0] S_AXI_WSTRB,
@@ -26,5 +32,31 @@ module axi4_lite_slave #(
     output reg S_AXI_BVALID,
     input S_AXI_BREADY
 );
-    
+  localparam    IDLE = 3'b000,
+                RADDR_CHANNEL = 3'b001,
+                RDATA_CHANNEL = 3'b010,
+                WRITE_CHANNEL = 3'b011,
+                WRESP_CHANNEL = 3'b100;
+
+  reg current_state, next_state;
+
+  reg [DATA_WIDTH - 1:0] mem[DATA_DEPTH - 1:0];
+  reg [ADDRESS_WIDTH - 1:0] address;
+
+  integer i;
+
+  always @(posedge ACLK or negedge ARESETN) begin
+    if (!ARESETN) current_state <= IDLE;
+    else current_state <= next_state;
+  end
+
+  always @(posedge ACLK or negedge ARESETN) begin
+    if (!ARESETN) begin
+      for (i = 0; i < DATA_DEPTH; i = i + 1) mem[i] <= 0;
+    end else begin
+      if (current_state == WRITE_CHANNEL) mem[S_AXI_AWADDR] <= S_AXI_WDATA;
+      else if (current_state == RADDR_CHANNEL) address <= S_AXI_ARADDR;
+    end
+  end
+
 endmodule
